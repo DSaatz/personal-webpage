a8167e08871b3be09318158c978d2a6e90193cc3
DSaatz <126864831+DSaatz@users.noreply.github.com>
2024-11-23 21:31:47 +0100

    .

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from '@/components/Hero'
 import Projects from '@/components/Projects'
-import Contact from '@/components/Contact'
 import SocialLinks from '@/components/SocialLinks'
 
 export default function Home() {
